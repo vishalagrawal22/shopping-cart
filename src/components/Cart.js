d6d8456d548d66cb964100afaa0e276017c58ec2
Vishal Agrawal <vishalagrawalva22@gmail.com>
2022-02-15 14:00:31 +0530

    Refactor useCart hook

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -8,59 +8,55 @@ function CartItem(product, quantity) {
 function useCart() {
   const [cartList, setCartList] = useState({});
 
-  let cart = (function (cartList, setCartList) {
-    let addItem = (product) => {
-      setCartList((oldCartList) => {
-        return { ...oldCartList, [product.id]: CartItem(product, 1) };
-      });
-    };
+  let addItem = (product) => {
+    setCartList((oldCartList) => {
+      return { ...oldCartList, [product.id]: CartItem(product, 1) };
+    });
+  };
 
-    let deleteItem = (product) => {
-      setCartList((oldCartList) => {
-        let newCartList = deepCopy(oldCartList);
-        delete newCartList[product.id];
-        return { ...newCartList };
-      });
-    };
+  let deleteItem = (product) => {
+    setCartList((oldCartList) => {
+      let newCartList = deepCopy(oldCartList);
+      delete newCartList[product.id];
+      return { ...newCartList };
+    });
+  };
 
-    let updateItem = (product, quantity) => {
-      setCartList((oldCartList) => {
-        return { ...oldCartList, [product.id]: CartItem(product, quantity) };
-      });
-    };
+  let updateItem = (product, quantity) => {
+    setCartList((oldCartList) => {
+      return { ...oldCartList, [product.id]: CartItem(product, quantity) };
+    });
+  };
 
-    let isItemInCart = (product) => {
-      if (cartList.hasOwnProperty(product.id)) {
-        return true;
-      } else {
-        return false;
-      }
-    };
+  let isItemInCart = (product) => {
+    if (cartList.hasOwnProperty(product.id)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
 
-    let getQuantity = (product) => {
-      if (isItemInCart(product)) {
-        return cartList[product.id].quantity;
-      } else {
-        return undefined;
-      }
-    };
+  let getQuantity = (product) => {
+    if (isItemInCart(product)) {
+      return cartList[product.id].quantity;
+    } else {
+      return undefined;
+    }
+  };
 
-    let getProducts = () => {
-      const products = cartList.map((cartItem) => cartItem.product);
-      return products;
-    };
+  let getProducts = () => {
+    const products = cartList.map((cartItem) => cartItem.product);
+    return products;
+  };
 
-    return {
-      addItem,
-      deleteItem,
-      updateItem,
-      isItemInCart,
-      getQuantity,
-      getProducts,
-    };
-  })(cartList, setCartList);
-
-  return cart;
+  return {
+    addItem,
+    deleteItem,
+    updateItem,
+    isItemInCart,
+    getQuantity,
+    getProducts,
+  };
 }
 
 function Cart() {
